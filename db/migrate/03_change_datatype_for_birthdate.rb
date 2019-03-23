77a9a91e24c8d3f1accc59615704833ae3e8cd54
Ravin Chhatrapati <ravinchhatrapati95@gmail.com>
2019-03-23 23:30:30 +0000

    Automatically backed up by Learn

--- db/migrate/03_change_datatype_for_birthdate.rb
+++ db/migrate/03_change_datatype_for_birthdate.rb
@@ -1,4 +1,8 @@
+<<<<<<< HEAD
 class ChangeDatatypeForBirthdate < ActiveRecord::Migration[5.1]
+=======
+class ChangeDatatypeForBirthdate
+>>>>>>> d98aac5b0e4aa1bafa603651713cfda800dbee15
 def change
 change_column :students, :birthdate, :datetime  # giving birthdte the data type of datetime instead of string
 end
